50af8a6b3c76dcfe05ed505827a02d8b0de95a12
Jake Morfe <jakemorfe@gmail.com>
2025-03-23 02:05:01 -0400

    package group id was incorrect
    made main class match skillapi's

---
+++ src/main/java/com/amuzil/omegasource/Avatar.java
@@ -0,0 +1,76 @@
+package com.amuzil.omegasource;
+
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+
+/**
+ * Note! This is a separate repository for a feature of Magus. As such,
+ * this file will be condensed into Magus', the modid will be changed, e.t.c.
+ * This is so Mahtaran and I can work on features of Magus separately.
+ */
+
+@Mod(Avatar.MOD_ID)
+public class Avatar {
+    // MOD ID reference
+    public static final String MOD_ID = "magus";
+    // Directly reference a log4j logger.
+    public static final Logger LOGGER = LogManager.getLogger();
+
+    public Avatar() {
+        // Register the setup method for mod loading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        // Register the enqueueIMC method for mod loading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
+        // Register the processIMC method for mod loading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
+        // Register the doClientStuff method for mod loading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+
+        // Register Testing Entities
+        // NOTE: This is strictly for testing and to be deleted later
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {}
+
+    private void doClientStuff(final FMLClientSetupEvent event) {
+        // do something that can only be done on the client
+        // Register the input modules
+    }
+
+    private void enqueueIMC(final InterModEnqueueEvent event) {}
+
+    private void processIMC(final InterModProcessEvent event) {}
+
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+        // Do something when the server starts
+        LOGGER.info("HELLO from server starting");
+    }
+
+    //Copied for 1.19
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+            // Some client setup code
+            LOGGER.info("HELLO FROM CLIENT SETUP");
+            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+        }
+    }
+}
