7b763920691bcbe2694e0c9ddf03aa230d811c88
Jake Morfe <jakemorfe@gmail.com>
2025-04-19 22:06:12 -0400

    stray print statement
    used regex to find it

--- src/main/java/com/amuzil/omegasource/bending/element/fire/FireStrikeSkill.java
+++ src/main/java/com/amuzil/omegasource/bending/element/fire/FireStrikeSkill.java
@@ -46,7 +46,6 @@ public class FireStrikeSkill extends BendingSkill {
         int lifetime = data.getTrait("lifetime", TimedTrait.class).getTime();
         double speed = data.getTrait("speed", SpeedTrait.class).getSpeed();
 
-        System.out.println(speed);
         ElementProjectile proj = new FireProjectile(entity, entity.level());
         proj.shoot(entity.getViewVector(1).x, entity.getViewVector(1).y, entity.getViewVector(1).z, (float) speed, 1);
         proj.setTimeToKill(lifetime);
