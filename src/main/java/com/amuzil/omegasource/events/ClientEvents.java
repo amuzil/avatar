42bd1e99271b2c6f96535a287058015185e20d06
Jake Morfe <jakemorfe@gmail.com>
2025-04-21 01:17:30 -0400

    Update ClientEvents.java

--- src/main/java/com/amuzil/omegasource/events/ClientEvents.java
+++ src/main/java/com/amuzil/omegasource/events/ClientEvents.java
@@ -4,16 +4,12 @@ import com.amuzil.omegasource.Avatar;
 import com.amuzil.omegasource.capability.Bender;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.ClientPlayerNetworkEvent;
-import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 
 @Mod.EventBusSubscriber(modid = Avatar.MOD_ID, value = Dist.CLIENT)
 public class ClientEvents {
-
-//    @SubscribeEvent
-//    public static void onClientLogin(PlayerEvent.PlayerLoggedInEvent event) {}
 
     @SubscribeEvent
     public static void onClientLogout(ClientPlayerNetworkEvent.LoggingOut event) {
