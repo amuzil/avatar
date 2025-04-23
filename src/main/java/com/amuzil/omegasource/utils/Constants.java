50a9f5249635af23c6c42392da9941ef98fc5a23
Jake Morfe <jakemorfe@gmail.com>
2025-04-22 21:43:30 -0400

    Create Constants.java

---
+++ src/main/java/com/amuzil/omegasource/utils/Constants.java
@@ -0,0 +1,14 @@
+package com.amuzil.omegasource.utils;
+
+import com.amuzil.omegasource.Avatar;
+
+
+public final class Constants {
+    // This class is for storing constant values and strings
+
+    public static final String MOD_ID = Avatar.MOD_ID;
+
+    private Constants() {
+        // Private constructor to prevent instantiation
+    }
+}
