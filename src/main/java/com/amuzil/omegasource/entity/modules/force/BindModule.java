f556bc7129f65ca14306ccbb2b21e46c644c948a
Jake Morfe <jakemorfe@gmail.com>
2025-06-13 16:19:32 -0400

    Create BindModule.java

---
+++ src/main/java/com/amuzil/omegasource/entity/modules/force/BindModule.java
@@ -0,0 +1,37 @@
+package com.amuzil.omegasource.entity.modules.force;
+
+import com.amuzil.omegasource.entity.AvatarEntity;
+import com.amuzil.omegasource.entity.modules.IForceModule;
+import net.minecraft.nbt.CompoundTag;
+
+
+public class BindModule implements IForceModule {
+
+    String id = "bind";
+
+    @Override
+    public String id() {
+        return id;
+    }
+
+    @Override
+    public void init(AvatarEntity entity) {
+
+    }
+
+    @Override
+    public void tick(AvatarEntity entity) {
+        entity.setDeltaMovement(entity.owner().position());
+        System.out.println("BindModule.tick: " + entity.getId() + " at " + entity.position());
+    }
+
+    @Override
+    public void save(CompoundTag nbt) {
+        nbt.putString("ID", id);
+    }
+
+    @Override
+    public void load(CompoundTag nbt) {
+        this.id = nbt.getString("ID");
+    }
+}
