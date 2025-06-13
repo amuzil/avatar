b692d6af6ddbb9455b9c1587fc4ac5f5ce384a95
Jake Morfe <jakemorfe@gmail.com>
2025-06-13 16:02:09 -0400

    fixed data sync issue

--- src/main/java/com/amuzil/omegasource/entity/modules/render/ParticleModule.java
+++ src/main/java/com/amuzil/omegasource/entity/modules/render/ParticleModule.java
@@ -1,9 +1,6 @@
 package com.amuzil.omegasource.entity.modules.render;
 
-import com.amuzil.omegasource.Avatar;
 import com.amuzil.omegasource.api.magus.skill.traits.skilltraits.BooleanTrait;
-import com.amuzil.omegasource.api.magus.skill.traits.skilltraits.LevelTrait;
-import com.amuzil.omegasource.api.magus.skill.traits.skilltraits.StringTrait;
 import com.amuzil.omegasource.bending.element.Element;
 import com.amuzil.omegasource.entity.AvatarEntity;
 import com.amuzil.omegasource.entity.modules.IRenderModule;
@@ -12,11 +9,6 @@ import com.lowdragmc.photon.client.fx.EntityEffect;
 import com.lowdragmc.photon.client.fx.FX;
 import com.lowdragmc.photon.client.fx.FXHelper;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.entity.Entity;
-
-import static com.amuzil.omegasource.Avatar.air_perma;
-import static com.amuzil.omegasource.Avatar.fire_bloom_perma;
 
 
 public class ParticleModule implements IRenderModule {
@@ -30,12 +22,11 @@ public class ParticleModule implements IRenderModule {
     @Override
     public void init(AvatarEntity entity) {
         Element element = entity.element();
-        StringTrait fxName = entity.getTrait(Constants.FX, StringTrait.class);
 
-//        if (entity.level().isClientSide && fxName != null) {
-//            FX fx = FXHelper.getFX(ResourceLocation.fromNamespaceAndPath(Avatar.MOD_ID, fxName.getInfo()));
+//        if (entity.level().isClientSide && entity.fxLocation() != null) {
+//            FX fx = FXHelper.getFX(entity.fxLocation());
 //            if (fx != null) {
-//                System.out.println("ParticleModule.init " + entity.getId());
+//                System.out.println(fx);
 //                EntityEffect entityEffect = new EntityEffect(fx, entity.level(), entity);
 //                entityEffect.start();
 //            }
@@ -45,12 +36,9 @@ public class ParticleModule implements IRenderModule {
     @Override
     public void tick(AvatarEntity entity) {
         // For starting effects per tick
-        Element element = entity.element();
-        StringTrait fxName = entity.getTrait(Constants.FX, StringTrait.class);
         BooleanTrait booleanTrait = entity.getTrait(Constants.ONE_SHOT, BooleanTrait.class);
-        if (!entity.level().isClientSide && fxName != null) {
-//            System.out.println("ParticleModule.tick Server-Side: " + !entity.level().isClientSide);
-            FX fx = FXHelper.getFX(ResourceLocation.fromNamespaceAndPath(Avatar.MOD_ID, fxName.getInfo()));
+        if (entity.level().isClientSide && entity.fxLocation() != null) {
+            FX fx = FXHelper.getFX(entity.fxLocation());
             if (fx != null) {
                 EntityEffect entityEffect = new EntityEffect(fx, entity.level(), entity);
                 entityEffect.start();
