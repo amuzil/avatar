686d989bf93bf6ae76a855705b5ee077680c6a1c
David Jake Morfe <jakemorfe@gmail.com>
2025-11-13 21:21:15 -0500

    bump

--- src/main/java/com/amuzil/av3/events/ServerEvents.java
+++ src/main/java/com/amuzil/av3/events/ServerEvents.java
@@ -95,7 +95,6 @@ public class ServerEvents {
     @SubscribeEvent
     public static void onServerTick(ServerTickEvent.Pre event) {
         NeoForge.EVENT_BUS.post(new SkillTickEvent());
-
     }
 
 }
