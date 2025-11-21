0fe13a499249fbf2d3f757bbe2689a126bea5e73
David Jake Morfe <jakemorfe@gmail.com>
2025-11-21 03:01:20 -0500

    started implementing EarthWallSkill

--- src/main/java/com/amuzil/av3/bending/element/earth/EarthWallSkill.java
+++ src/main/java/com/amuzil/av3/bending/element/earth/EarthWallSkill.java
@@ -3,22 +3,29 @@ package com.amuzil.av3.bending.element.earth;
 import com.amuzil.av3.Avatar;
 import com.amuzil.av3.bending.skill.EarthSkill;
 import com.amuzil.av3.data.capability.Bender;
+import com.amuzil.av3.entity.construct.AvatarRigidBlock;
 import com.amuzil.av3.utils.Constants;
+import com.amuzil.magus.skill.data.SkillData;
 import com.amuzil.magus.skill.data.SkillPathBuilder;
-import com.amuzil.magus.skill.traits.skilltraits.KnockbackTrait;
 import com.amuzil.magus.skill.traits.skilltraits.SizeTrait;
+import com.amuzil.magus.skill.traits.skilltraits.StringTrait;
+import com.amuzil.magus.skill.traits.skilltraits.TimedTrait;
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
 
 import static com.amuzil.av3.bending.form.BendingForms.RAISE;
-import static com.amuzil.av3.utils.bending.SkillHelper.canEarthBend;
+import static com.amuzil.av3.utils.bending.SkillHelper.*;
 
 
 public class EarthWallSkill extends EarthSkill {
     // TODO: Create Earth Wall Skill that adjusts shape based on player's look angle
     public EarthWallSkill() {
         super(Avatar.MOD_ID, "earth_wall");
-        addTrait(new KnockbackTrait(Constants.KNOCKBACK, 1.5f));
-        addTrait(new SizeTrait(Constants.SIZE, 1.0f));
+        addTrait(new StringTrait(Constants.FX, "earth_wall"));
+        addTrait(new TimedTrait(Constants.LIFETIME, 500));
+        addTrait(new SizeTrait(Constants.SIZE, 3.0f));
 
         this.startPaths = SkillPathBuilder.getInstance()
                 .add(RAISE)
@@ -27,17 +34,36 @@ public class EarthWallSkill extends EarthSkill {
 
     @Override
     public void start(Bender bender) {
+        super.start(bender);
         LivingEntity entity = bender.getEntity();
         if (!canEarthBend(entity)) return; // Can't earth bend if too far from ground
-    }
+        Level level = bender.getEntity().level();
+        SkillData data = bender.getSkillData(this);
+        BlockPos blockPos = bender.getEntity().blockPosition().below();
+        BlockState blockState = level.getBlockState(blockPos);
 
-    @Override
-    public void run(Bender bender) {
-        super.run(bender);
-    }
+        int lifetime = data.getTrait(Constants.LIFETIME, TimedTrait.class).getTime();
+        double size = data.getTrait(Constants.SIZE, SizeTrait.class).getSize();
 
-    @Override
-    public void stop(Bender bender) {
-        super.stop(bender);
+        AvatarRigidBlock rigidBlock = new AvatarRigidBlock(level);
+        rigidBlock.setElement(element());
+        rigidBlock.setFX(skillData.getTrait(Constants.FX, StringTrait.class).getInfo());
+        rigidBlock.setBlockState(blockState);
+        rigidBlock.setPos(getPivot(entity, 3f));
+        rigidBlock.getRigidBody().setMass(50f);
+        rigidBlock.getRigidBody().setAngularFactor(0f);
+        rigidBlock.setOwner(entity);
+        rigidBlock.setMaxLifetime(lifetime);
+        rigidBlock.setWidth((float) size);
+        rigidBlock.setHeight((float) size);
+        rigidBlock.setDamageable(false);
+
+        rigidBlock.init();
+
+        bender.formPath.clear();
+        data.setSkillState(SkillState.IDLE);
+
+        bender.getSelection().addEntityId(rigidBlock.getUUID());
+        entity.level().addFreshEntity(rigidBlock);
     }
 }
