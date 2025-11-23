89f17dbc08b6c10fc51bafb9e76b538d5e06b5dc
John Atkins <johnmatkins8@gmail.com>
2025-11-23 14:02:53 +1100

    removed extra constructor

--- src/main/java/com/amuzil/av3/entity/construct/AvatarElementCollider.java
+++ src/main/java/com/amuzil/av3/entity/construct/AvatarElementCollider.java
@@ -2,7 +2,6 @@ package com.amuzil.av3.entity.construct;
 
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.phys.Vec3;
 
 
 // Used for fire, water, and air. RigidBlock is earth.
@@ -10,10 +9,6 @@ public class AvatarElementCollider extends AvatarRigidBlock {
 
     public AvatarElementCollider(EntityType<? extends AvatarRigidBlock> type, Level level) {
         super(type, level);
-    }
-
-    public AvatarElementCollider(Level level, Vec3 pos, double width, double height, double depth) {
-        super(level, pos, width, height, depth);
     }
 
     public AvatarElementCollider(Level level) {
