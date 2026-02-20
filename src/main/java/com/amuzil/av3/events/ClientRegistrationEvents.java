19711abc554256f2003afbbff686bb344c9cee05
David Jake Morfe <jakemorfe@gmail.com>
2026-02-20 01:26:49 -0500

    Started CompoundCollisionShape impl and created RigidBlockFactory

--- src/main/java/com/amuzil/av3/events/ClientRegistrationEvents.java
+++ src/main/java/com/amuzil/av3/events/ClientRegistrationEvents.java
@@ -25,6 +25,7 @@ public class ClientRegistrationEvents {
         event.registerEntityRenderer(AvatarEntities.AVATAR_WATER_PROJECTILE_ENTITY_TYPE.get(), MarchingCubesEntityRenderer::new);
 
         event.registerEntityRenderer(AvatarEntities.AVATAR_RIGID_BLOCK_ENTITY_TYPE.get(), PhysicsBlockRenderer::new);
+        event.registerEntityRenderer(AvatarEntities.COMPOUND_RIGID_BLOCK_ENTITY_TYPE.get(), PhysicsBlockRenderer::new);
 
         event.registerEntityRenderer(AvatarEntities.AVATAR_SKYBISON_ENTITY_TYPE.get(), SkyBisonRenderer::new);
     }
