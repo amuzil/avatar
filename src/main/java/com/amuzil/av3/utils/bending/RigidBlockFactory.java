6c06384af49a4f3de0decc24df8d95f6ee673f35
David Jake Morfe <jakemorfe@gmail.com>
2026-03-04 00:16:07 -0500

    make createGlueJoint method
    and reorg

--- src/main/java/com/amuzil/av3/utils/bending/RigidBlockFactory.java
+++ src/main/java/com/amuzil/av3/utils/bending/RigidBlockFactory.java
@@ -1,7 +1,14 @@
 package com.amuzil.av3.utils.bending;
 
 import com.amuzil.av3.entity.construct.AvatarRigidBlock;
+import com.amuzil.caliber.physics.bullet.collision.body.EntityRigidBody;
+import com.amuzil.caliber.physics.bullet.collision.space.MinecraftSpace;
 import com.amuzil.caliber.physics.bullet.math.Convert;
+import com.jme3.bullet.RotationOrder;
+import com.jme3.bullet.joints.New6Dof;
+import com.jme3.bullet.joints.motors.MotorParam;
+import com.jme3.math.Matrix3f;
+import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
@@ -14,15 +21,18 @@ import static com.amuzil.av3.utils.bending.SkillHelper.getRightPivot;
 
 public class RigidBlockFactory {
 
-    public static @NotNull AvatarRigidBlock createBlock(Level level, BlockState blockState, LivingEntity owner, int blockCount, int lifetime, float size) {
+    public static @NotNull AvatarRigidBlock createBlock(Level level, BlockState blockState, LivingEntity owner, int lifetime, float size) {
         AvatarRigidBlock rigidBlock = new AvatarRigidBlock(level);
         rigidBlock.setBlockState(blockState);
         rigidBlock.setOwner(owner);
-        rigidBlock.setPos(getRightPivot(owner, 1.0f, blockCount * -0.8));
+        rigidBlock.setYRot(owner.getYRot());
+        rigidBlock.setYHeadRot(owner.getYRot());
+        rigidBlock.yRotO = owner.getYRot(); // previous rotation too, prevents lerp snapping
+        rigidBlock.setXRot(0f); // ignore pitch, walls should stand upright
+        rigidBlock.getRigidBody().setPhysicsRotation(Convert.toBullet(0, owner.getYRot()));
         rigidBlock.setMaxLifetime(lifetime);
         rigidBlock.setWidth(size);
         rigidBlock.setHeight(size);
-        rigidBlock.getRigidBody().setPhysicsRotation(Convert.toBullet(owner.getXRot(), owner.getYRot()));
         rigidBlock.setDamageable(false);
         return rigidBlock;
     }
@@ -31,11 +41,11 @@ public class RigidBlockFactory {
         AvatarRigidBlock rigidBlock = new AvatarRigidBlock(level);
         rigidBlock.setBlockState(blockState);
         rigidBlock.setOwner(owner);
+        rigidBlock.getRigidBody().setPhysicsRotation(Convert.toBullet(owner.getXRot(), owner.getYRot()));
         rigidBlock.setPos(getRightPivot(owner, 1.0f, blockCount * -0.8));
         rigidBlock.setMaxLifetime(lifetime);
         rigidBlock.setWidth(size);
         rigidBlock.setHeight(size);
-        rigidBlock.getRigidBody().setPhysicsRotation(Convert.toBullet(owner.getXRot(), owner.getYRot()));
         rigidBlock.getRigidBody().setMass(0f);
         rigidBlock.getRigidBody().setKinematic(true);
         rigidBlock.getRigidBody().prioritize((Player) owner);
@@ -59,5 +69,32 @@ public class RigidBlockFactory {
         rigidBlock.setDamageable(false);
         rigidBlock.setRigidBodyDirty(true);
         return rigidBlock;
+    }
+
+    public static void createGlueJoint(MinecraftSpace space, AvatarRigidBlock blockA, AvatarRigidBlock blockB) {
+        EntityRigidBody rigidBodyA = blockA.getRigidBody();
+        EntityRigidBody rigidBodyB = blockB.getRigidBody();
+
+        Vector3f posA = rigidBodyA.getPhysicsLocation(null);
+        Vector3f posB = rigidBodyB.getPhysicsLocation(null);
+        Vector3f midpoint = posA.add(posB).mult(0.5f);
+        Vector3f pivotInA = midpoint.subtract(posA);
+        Vector3f pivotInB = midpoint.subtract(posB);
+
+        New6Dof glue = new New6Dof(
+                rigidBodyA, rigidBodyB,
+                pivotInA, pivotInB,
+                Matrix3f.IDENTITY, Matrix3f.IDENTITY,
+                RotationOrder.XYZ
+        );
+
+        for (int dof = 0; dof < 6; dof++) {
+            glue.set(MotorParam.LowerLimit, dof, 0f);
+            glue.set(MotorParam.UpperLimit, dof, 0f);
+        }
+
+        glue.setBreakingImpulseThreshold(60f); // lower = easier to break
+        glue.setCollisionBetweenLinkedBodies(false);
+        space.addJoint(glue);
     }
 }
